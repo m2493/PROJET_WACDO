c4002cefca1d8f73ceb82e941341b92ce6648f6b
mlv <mlv2493@gmail.com>
2026-05-14 17:28:55 +0200

    Modification pour mep

--- wacdo/src/main/java/com/marion/wacdo/configs/SecurityConfig.java
+++ wacdo/src/main/java/com/marion/wacdo/configs/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "http://localhost:5175"));
+        config.setAllowedOrigins(List.of("https://projet-wacdo.onrender.com","http://localhost:5173", "http://localhost:5174", "http://localhost:5175"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
