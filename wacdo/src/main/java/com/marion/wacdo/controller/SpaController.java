d16ba4bd994db8aa67fbc49d96d2958f7c714c11
mlv <mlv2493@gmail.com>
2026-05-14 19:08:54 +0200

    modif pour MEP2

--- wacdo/src/main/java/com/marion/wacdo/controller/SpaController.java
+++ wacdo/src/main/java/com/marion/wacdo/controller/SpaController.java
@@ -1,16 +1,12 @@
 package com.marion.wacdo.controller;
 
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SpaController {
 
-    @RequestMapping(value = {
-            "/{path:[^\\.]*}",
-            "/**/{path:[^\\.]*}"
-    })
+    @GetMapping(value = "/{path:[^\\.]*}")
     public String forward() {
         return "forward:/index.html";
     }
